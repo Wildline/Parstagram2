61a868885e4b216601772ba0ca1d13a86c97a0ef
Wildline <Wildlinelincifort11234@gmail.com>
2021-10-15 17:35:18 -0400

    Initial Commit

---
+++ Parstagram2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram2
+//
+//  Created by Wildline  Lincifort on 10/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
