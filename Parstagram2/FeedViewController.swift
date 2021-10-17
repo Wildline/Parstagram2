d3bf215f417078d265057f5344b178c409171e35
Wildline <Wildlinelincifort11234@gmail.com>
2021-10-16 22:59:16 -0400

    update

--- Parstagram2/FeedViewController.swift
+++ Parstagram2/FeedViewController.swift
@@ -184,7 +184,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         PFUser.logOut()
         
         let main = UIStoryboard(name: "Main", bundle: nil)
-        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewContrller")
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
         
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let
                 delegate = windowScene.delegate as? SceneDelegate else { return }
